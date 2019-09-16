8ab87862fe3ef7107f99db5c546fec0af547ad69
Okita <keisi1100525@outlook.jp>
2019-09-16 18:39:09 +0900

    ✨Make Page Routing and Dynamic Route Matching

---
+++ pages/app/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        pages/app/index.vue
+
+        <p>
+            <nuxt-link to="/app">/app</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/settings">/app/settings</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/attention-card">/app/attention-card</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/attention-me">/app/attention-me</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/edit-profile">/app/edit-profile</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/chat">/chat</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/chat/jieajf">/chat/jieajf</nuxt-link>
+        </p>
+    </div>
+</template>
---
+++ pages/chat/_room_id.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        pages/chat/{room_id}
+        <br />
+        {{ $route.params.room_id }}
+        <p>
+            <nuxt-link to="/app">/app</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/settings">/app/settings</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/attention-card">/app/attention-card</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/attention-me">/app/attention-me</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/app/edit-profile">/app/edit-profile</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/chat">/chat</nuxt-link>
+        </p>
+
+        <p>
+            <nuxt-link to="/chat/jieajf">/chat/jieajf</nuxt-link>
+        </p>
+    </div>
+</template>
