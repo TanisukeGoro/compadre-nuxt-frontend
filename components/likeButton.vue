9683e430804b96ef368cf6974ebbb8dfeeb7a72f
Kei Nakanowatari <nakanowatari.key@gmail.com>
2019-09-23 17:14:21 +0900

    components is like Button in if you need check it out and  i don't decide color yet

---
+++ components/likeButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center">
+        <v-btn
+            rounded
+            dark
+            style="background: linear-gradient(to right, #70e1f5, #ffd194);"
+            >Rounded Button</v-btn
+        >
+    </div>
+</template>
--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,15 +1,19 @@
 <template>
     <v-content>
         <SelectCard />
+        <likeButton />
     </v-content>
 </template>
 
 <script>
 import SelectCard from '~/components/SelectCard'
 // import { firebase, db } from '~/plugins/firebase'
+// eslint-disable-next-line no-unused-vars
+import likeButton from '~/components/likeButton'
 export default {
     components: {
-        SelectCard
+        SelectCard,
+        likeButton
     },
     methods: {
         async logout() {
