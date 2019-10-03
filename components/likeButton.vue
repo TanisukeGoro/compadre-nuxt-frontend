01e8b42d25d67060f85c46ee23708a17d363d421
yoshidadaiki <daicam440@gmail.com>
2019-10-03 10:58:03 +0900

    likebuttonだけ後から変更をステージング

--- components/likeButton.vue
+++ components/likeButton.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="text-center">
-        <v-btn
-            rounded
-            dark
-            style="background: linear-gradient(to right, #70e1f5, #ffd194);"
-            >Rounded Button</v-btn
-        >
-    </div>
+    <v-row>
+        <v-layout justify-center>
+            <v-btn
+                rounded
+                dark
+                class="mb-5 btn"
+                style="background: linear-gradient(to right, #70e1f5, #ffd194);"
+                @click="$emit('ClickLikebutton')"
+                >Hi!</v-btn
+            >
+        </v-layout>
+    </v-row>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
