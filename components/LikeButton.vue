c18bb51014c2134091213fa8fef6d2beb82a2096
yoshidadaiki <daicam440@gmail.com>
2019-10-06 18:24:39 +0900

    Hiボタンコンポーネント再作成

---
+++ components/LikeButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-row>
+        <v-layout justify-center class="btn">
+            <v-btn
+                rounded
+                dark
+                class="mb-5 btn"
+                style="background: linear-gradient(to right, #70e1f5, #ffd194);"
+                @click="$emit('ClickLikebutton')"
+                >Hi!</v-btn
+            >
+        </v-layout>
+    </v-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.btn {
+    padding-top: 50px;
+}
+</style>
