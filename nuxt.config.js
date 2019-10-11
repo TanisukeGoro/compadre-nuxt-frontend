08482ddc2937f7f7f61452381670baa7577284de
Okita <keisi1100525@outlook.jp>
2019-10-12 01:42:17 +0900

    fix modifide

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,13 +31,69 @@ export default {
                 content: 'notranslate'
             }
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-          { rel: "apple-touch-startup-image", href: "launch-1125x2436.png", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" },
-            { rel: "apple-touch-startup-image", href: "launch-750x1334.png", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" },
-            { rel: "apple-touch-startup-image", href: "launch-1242x2208.png", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" },
-            { rel: "apple-touch-startup-image", href: "launch-640x1136.png", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" }
-    ]
-        
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphone5_splash.png',
+                media:
+                    '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphone6_splash.png',
+                media:
+                    '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphoneplus_splash.png',
+                media:
+                    '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphonex_splash.png',
+                media:
+                    '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphonexr_splash.png',
+                media:
+                    '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/iphonexsmax_splash.png',
+                media:
+                    '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/ipad_splash.png',
+                media:
+                    '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/ipadpro1_splash.png',
+                media:
+                    '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/ipadpro3_splash.png',
+                media:
+                    '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
+            },
+            {
+                rel: 'apple-touch-startup-image',
+                href: '/splashscreens/ipadpro2_splash.png',
+                media:
+                    '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
+            }
+        ]
     },
     /*
      ** Customize the progress-bar color
