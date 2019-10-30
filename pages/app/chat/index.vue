8ffd14b4a804da1106aa271d758a66681ad30218
Okita <keisi1100525@outlook.jp>
2019-10-31 07:13:44 +0900

    💫 Add Chat Translation Feature

--- pages/app/chat/index.vue
+++ pages/app/chat/index.vue
@@ -51,11 +51,13 @@ export default {
     components: {
         NoChatPartner
     },
-    data: () => ({
-        // AWSの画像だったら爆速で取ってこれる感じか。
-        iconURL: `${process.env.AwsStoreImageUrl}images/GDayPwYX4Ioeknxb6R6Dbn9eDHXdr2NNy94Dctp5.jpeg`,
-        No_chat_partner: true
-    }),
+    data() {
+        return {
+            // AWSの画像だったら爆速で取ってこれる感じか。
+            iconURL: `${process.env.AwsStoreImageUrl}images/GDayPwYX4Ioeknxb6R6Dbn9eDHXdr2NNy94Dctp5.jpeg`,
+            No_chat_partner: true
+        }
+    },
     computed: {
         ...mapGetters('app/chat/chatList', ['chatLists'])
     },
